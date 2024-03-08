79ac64c48e5155ebbc5f672aeb8308dc83e08184
SeifAwadalla <seifsalama10@gmail.com>
2024-03-08 20:12:57 +0000

    got image generator working and wokring on background change

--- src/stability_AI.py
+++ src/stability_AI.py
@@ -1,20 +1,45 @@
+import base64
 import os
 import requests
 
-api_host = os.getenv('API_HOST', 'https://api.stability.ai')
-url = f"{api_host}/v1/user/account"
+from dotenv import load_dotenv
 
+load_dotenv()
+
+engine_id = "stable-diffusion-v1-6"
+api_host = os.getenv('API_HOST', 'https://api.stability.ai')
 api_key = os.getenv("STABILITY_API_KEY")
+prompt = "A British male in his late twenties wearing gamer fashion and is really into movie culture on a live stream face cam speaking to a microphone with his room in the background"
+
 if api_key is None:
     raise Exception("Missing Stability API key.")
 
-response = requests.get(url, headers={
-    "Authorization": f"Bearer {api_key}"
-})
+response = requests.post(
+    f"{api_host}/v1/generation/{engine_id}/text-to-image",
+    headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    },
+    json={
+        "text_prompts": [
+            {
+                "text": prompt
+            }
+        ],
+        "cfg_scale": 7,
+        "height": 1024,
+        "width": 1024,
+        "samples": 1,
+        "steps": 30,
+    },
+)
 
 if response.status_code != 200:
     raise Exception("Non-200 response: " + str(response.text))
 
-# Do something with the payload...
-payload = response.json()
+data = response.json()
 
+for i, image in enumerate(data["artifacts"]):
+    with open(f"./out/{prompt.replace(' ','_')}.png", "wb") as f:
+        f.write(base64.b64decode(image["base64"]))
