230b81437158f4bf2e27585091432d54fe2edab7
Tiberiu Aurelian Țoca <78236499+ReverseZoom2151@users.noreply.github.com>
2024-03-09 16:25:42 +0000

    finalised text to speech

--- src/persona.py
+++ src/persona.py
@@ -9,7 +9,6 @@ load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-
 def ask_gpt(prompt: str) -> str:
     chat_log = [{"role": "system", "content": "You are a white British male in your late 20s, funny, with a strong passion for gaming, also deeply knowledgeable about movie culture, close to an entertainment nerd, an individual who skipped university to pursue livestreaming, taking it a step further from just a simple hobby to a full-time job. Read and react to the user chat informally and with a sense of humor."},
                 {"role": "user", "content": prompt}]
@@ -20,12 +19,10 @@ def ask_gpt(prompt: str) -> str:
     )
     return response.choices[0].message.content 
 
-
 def read_chat_log(chat_log_file):
     with open(chat_log_file, "r", encoding="utf-8") as file:
         chat_log_content = file.read()
     return chat_log_content
-
 
 def process_chat_log(chat_log_content):
     chat_msgs = chat_log_content.strip().split('\n')
@@ -33,16 +30,13 @@ def process_chat_log(chat_log_content):
 
     if chat_msgs:
         latest_msg = chat_msgs[-1]
-
         user, content = latest_msg.split(':', 1)
         prompt = content
         response = emoji.demojize(ask_gpt(prompt))
         print(f"{latest_msg}\nAurora: {response}\n")
-        
         texttospeech(response)
         f.write(response)
         f.close()
-
 
 def main() -> None:
     
