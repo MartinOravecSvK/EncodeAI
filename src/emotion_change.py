fbf868474b0d62c92d38e41a64ea64383a073757
SeifAwadalla <seifsalama10@gmail.com>
2024-03-10 02:44:29 +0000

    emotion change loop

--- src/emotion_change.py
+++ src/emotion_change.py
@@ -12,35 +12,37 @@ api_key = os.getenv("STABILITY_API_KEY")
 
 img_prompt = "A British male in his late twenties wearing gamer fashion and is really into movie culture on a live stream face cam speaking to a microphone"
 
-emotion_prompt="Make him angry"
+emotion_prompts=[("Make him angry","angry"),("Make him have a neutral expression","neutral"),("Make him sad","sad"),("Make him laugh","laughing"),("Make him confused","confused"),("Make him cringe","cringing"),("Make him excited","excited"),("Make him shocked","shocked")]
 
 if api_key is None:
     raise Exception("Missing Stability API key.")
 
-response = requests.post(
-    f"{api_host}/v1/generation/{engine_id}/image-to-image",
-    headers={
-        "Accept": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    },
-    files={
-        "init_image": open(f"./out/{img_prompt.replace(' ','_')}_with_bg.png", "rb")
-    },
-    data={
-        "image_strength": 0.55,
-        "init_image_mode": "IMAGE_STRENGTH",
-        "text_prompts[0][text]": emotion_prompt,
-        "cfg_scale": 14,
-        "samples": 1,
-        "steps": 30,
-    }
-)
+for i in emotion_prompts:
 
-if response.status_code != 200:
-    raise Exception("Non-200 response: " + str(response.text))
+    response = requests.post(
+        f"{api_host}/v1/generation/{engine_id}/image-to-image",
+        headers={
+            "Accept": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        },
+        files={
+            "init_image": open("./out/happy_guy.png", "rb")
+        },
+        data={
+            "image_strength": 0.6,
+            "init_image_mode": "IMAGE_STRENGTH",
+            "text_prompts[0][text]": i[0],
+            "cfg_scale": 30,
+            "samples": 1,
+            "steps": 30,
+        }
+    )
 
-data = response.json()
+    if response.status_code != 200:
+        raise Exception("Non-200 response: " + str(response.text))
 
-for i, image in enumerate(data["artifacts"]):
-    with open(f"./out/{img_prompt.replace(' ','_')}_emotion_change.png", "wb") as f:
-        f.write(base64.b64decode(image["base64"]))
+    data = response.json()
+
+    for j, image in enumerate(data["artifacts"]):
+        with open(f"./out/{i[1]}_guy.png", "wb") as f:
+            f.write(base64.b64decode(image["base64"]))
